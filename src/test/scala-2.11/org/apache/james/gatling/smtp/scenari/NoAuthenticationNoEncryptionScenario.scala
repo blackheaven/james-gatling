3a8d7f209d96cd899f229ef5cc794fc86d6f0b5b
Gautier DI FOLCO <gdifolco@linagora.com>
2018-09-07 13:01:28 +0200

    Idem broken case

--- src/test/scala-2.11/org/apache/james/gatling/smtp/scenari/NoAuthenticationNoEncryptionScenario.scala
+++ src/test/scala-2.11/org/apache/james/gatling/smtp/scenari/NoAuthenticationNoEncryptionScenario.scala
@@ -6,24 +6,39 @@ import org.apache.james.gatling.jmap.scenari.common.Configuration._
 import org.apache.james.gatling.smtp.PreDef._
 
 import scala.concurrent.Await
+import scala.concurrent.Awaitable
 import scala.concurrent.duration._
+import scala.util.Try
 
 class NoAuthenticationNoEncryptionScenario extends Simulation {
 
   val users = new UserCreator(BaseJamesWebAdministrationUrl).createUsersWithInboxAndOutbox(UserCount)
   val queues = new QueueRequester(BaseJamesWebAdministrationUrl)
 
+  private def waitOrElse[T](awaitable: Awaitable[T], atMost: Duration, orElse: T): T = {
+    try {
+      Await.result(awaitable, atMost)
+    } catch {
+      case _ : Throwable => orElse
+    }
+  }
+
   val scn = scenario("SMTP_No_Authentication_No_Encryption")
     .feed(UserFeeder.createCompletedUserFeederWithInboxAndOutbox(users))
     .pause(1 second)
-    .during(30 second) {
+    .group("Queuing") {
+      during(30 second) {
         exec(smtp("sendMail")
             .subject("subject")
             .body("This is the mail body being sent"))
           .pause(1 second)
-      }.asLongAs(session => !Await.result(queues.areMailQueuesEmpty(), 1 second)) {
-        pause(0.5 second)
       }
+    }
+    .group("Dequeuing") {
+      asLongAs(session => !waitOrElse(queues.areMailQueuesEmpty(), 2 seconds,  false)) {
+        pause(0.5 seconds)
+      }
+    }
 
   setUp(scn.inject(nothingFor(10 seconds), rampUsers(UserCount) over(10 seconds))).protocols(smtp)
 }
